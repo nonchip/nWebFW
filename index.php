f287a867fdd5fc8d8e5ad66e38d2cb7fc038938c
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2014-01-06 01:54:50 +0100

    added _DEBUG

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
+$prof_start_time=microtime(true);
 
 // bootstrap config start
 
+define('_DEBUG',        true                );
 define('_APP_PATH',     __DIR__.'/app'      );
 define('_SYS_PATH',     __DIR__.'/nwebfw'   );
 
 // bootstrap config end
 
 include(_SYS_PATH.'/main.php');
+
+$prof_end_time=microtime(true);
+
+if(_DEBUG){
+    echo 'Time: '.($prof_end_time-$prof_start_time).'  Mem: '.memory_get_peak_usage();
+}

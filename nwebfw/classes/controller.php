51aacc03c3c82ace38c7509544d1256f45df4efc
Michael Zimmer <me@nonchip.de>
2014-01-07 16:17:08 +0000

    Update controller.php

--- nwebfw/classes/controller.php
+++ nwebfw/classes/controller.php
@@ -5,6 +5,7 @@ class Controller{
     public function module($name){
         if(file_exists(_APP_PATH.'/modules/'.$name.'.php')){
             include_once(_APP_PATH.'/modules/'.$name.'.php');
+            $name='\\App\\modules\\'.ucfirst($name);
             if(class_exists($name))
                 return new $name();
         }

9232edf42c120a707147c25c36307be41e6779b5
Michael Zimmer <me@nonchip.de>
2014-01-08 15:56:09 +0000

    rolling back bullshit

--- nwebfw/helpers.php
+++ nwebfw/helpers.php
@@ -2,11 +2,9 @@
 
 function h_request_path()
 {
-    $web_uri = explode('/', trim(_WEB_PATH, '/'));
     $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
     $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
-    $parts2 = array_diff_assoc($request_uri, $web_uri);
-    $parts = array_diff_assoc($parts, $script_name);
+    $parts = array_diff_assoc($request_uri, $script_name);
     if (empty($parts))
     {
         return '/';

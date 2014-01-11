d916d49dc6ecd4f1854ca412fa4911bbefd04a80
Michael Zimmer <me@nonchip.de>
2014-01-11 12:20:39 +0000

    Update helpers.php

--- nwebfw/helpers.php
+++ nwebfw/helpers.php
@@ -1,18 +1,20 @@
 <?php
 
-function h_request_path()
-{
-    $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
-    $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
-    $parts = array_diff_assoc($request_uri, $script_name);
-    if (empty($parts))
+if(!function_exists('h_request_path')){
+    function h_request_path()
     {
-        return '/';
+        $request_uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+        $script_name = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+        $parts = array_diff_assoc($request_uri, $script_name);
+        if (empty($parts))
+        {
+            return '/';
+        }
+        $path = implode('/', $parts);
+        if (($position = strpos($path, '?')) !== FALSE)
+        {
+            $path = substr($path, 0, $position);
+        }
+        return $path;
     }
-    $path = implode('/', $parts);
-    if (($position = strpos($path, '?')) !== FALSE)
-    {
-        $path = substr($path, 0, $position);
-    }
-    return $path;
 }

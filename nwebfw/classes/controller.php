4e3531f93bf972bcf851f9b93bb0f8cdd8101a08
Michael Zimmer <me@nonchip.de>
2014-04-12 07:57:12 +0000

    Update controller.php

--- nwebfw/classes/controller.php
+++ nwebfw/classes/controller.php
@@ -2,7 +2,7 @@
 namespace n\c;
 
 class Controller{
-    public function module($name){
+    public function widget($name){
         if (file_exists(_APP_PATH . '/widgets/' . $name . '.php')) {
             include_once(_APP_PATH . '/widgets/' . $name . '.php');
             $name = '\\App\\widgets\\' . ucfirst($name);
